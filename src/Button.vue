a171a2735d1f5ef9bdf29f203994c064c28e9a9c
zhang4417 <1804815164@qq.com>
2020-04-21 21:48:14 +0800

    给Button组件添加基本样式

--- src/Button.vue
+++ src/Button.vue
@@ -1,9 +1,25 @@
 <template>
-    <button>hi</button>
+  <button class="g-button">按钮</button>
 </template>
 <script>
-    export default{}
+export default {};
 </script>
 <style>
-
+.g-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background-color: var(--button-bg);
+}
+.g-button:focus {
+  outline: none;
+}
+.g-button:hover {
+  border-color: var(--border-color-hover);
+}
+.g-button:active {
+  background: var(--button-active-bg);
+}
 </style>
