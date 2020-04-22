e3a34f2dbfbfd024e6199a3e6513446a015392cc
zhang4417 <1804815164@qq.com>
2020-04-22 22:21:21 +0800

    测试参数icon iconPosition loading

--- src/app.js
+++ src/app.js
@@ -14,3 +14,45 @@ new Vue({
         loading3: false,
     }
 })
+
+import chai from 'chai'
+const expect = chai.expect
+
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: { icon: 'setting' }
+    }).$mount()
+    let useElement = vm.$el.querySelector('use')
+    let attr = useElement.getAttribute('xlink:href')
+    expect(attr).to.eq('#i-setting')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting',
+            iconPosition: 'right'
+        }
+    }).$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let { order } = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: { icon: 'setting', loading: true }
+    }).$mount()
+    let useElement = vm.$el.querySelector('use')
+    let attr = useElement.getAttribute('xlink:href')
+    expect(attr).to.eq('#i-loading')
+    vm.$el.remove()
+    vm.$destroy()
+}
