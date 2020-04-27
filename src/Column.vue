137f6a848278174f5789d0c2198d2a80893fd798
zhang4417 <1804815164@qq.com>
2020-04-27 17:21:00 +0800

    使用SCSS语法@for自定义网格宽度和间距

--- src/Column.vue
+++ src/Column.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="col">
+  <div class="col" :class="`col-${span} right-${space}`">
     <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Column"
+  name: "Column",
+  props: {
+    span: {
+      type: [String, Number]
+    },
+    space: {
+      type: [String, Number]
+    }
+  }
 };
 </script>
 
@@ -15,5 +23,16 @@ export default {
   height: 100px;
   width: 50%;
   border: 1px solid red;
+
+  @for $n from 1 through 24 {
+    &.col-#{$n} {
+      width: $n/24 * 100%;
+    }
+  }
+  @for $n from 1 through 24 {
+    &.right-#{$n} {
+      margin-right: $n/24 * 100%;
+    }
+  }
 }
 </style>
