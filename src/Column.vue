f5757d2fba79a1651f1e255424be015187d13135
zhang4417 <1804815164@qq.com>
2020-04-27 16:33:27 +0800

    创建p-row  p-col的组件

---
+++ src/Column.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="col">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Column"
+};
+</script>
+
+<style lang="scss" scoped>
+.col {
+  height: 100px;
+  width: 50%;
+  border: 1px solid red;
+}
+</style>
--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import Button from './Button.vue'
 import Icon from './Icon.vue'
 import ButtonGroup from './ButtonGroup.vue'
 import Input from './Input.vue'
+import Row from './Row.vue'
+import Column from './Column.vue'
 import '../test.js'
 import './svg.js'
 
@@ -10,6 +12,8 @@ Vue.component('p-button', Button)
 Vue.component('p-icon', Icon)
 Vue.component('button-group', ButtonGroup)
 Vue.component('p-input', Input)
+Vue.component('p-row', Row)
+Vue.component('p-col', Column)
 new Vue({
     el: '#app',
     data: {
